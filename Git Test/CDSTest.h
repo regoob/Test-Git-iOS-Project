3ffe1a9d751ee78d5385b03c7a67fa598afa5df0
Brian Goo <bgoo@coxds.com>
2014-03-04 14:21:24 -0800

    added a test class

---
+++ Git Test/CDSTest.h
@@ -0,0 +1,13 @@
+//
+//  CDSTest.h
+//  Git Test
+//
+//  Created by Brian Goo on 3/4/14.
+//  Copyright (c) 2014 Cox Digital Solutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CDSTest : NSObject
+
+@end
---
+++ Git Test/CDSTest.m
@@ -0,0 +1,13 @@
+//
+//  CDSTest.m
+//  Git Test
+//
+//  Created by Brian Goo on 3/4/14.
+//  Copyright (c) 2014 Cox Digital Solutions. All rights reserved.
+//
+
+#import "CDSTest.h"
+
+@implementation CDSTest
+
+@end
